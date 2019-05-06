38cba0b8982338b3641116f5ce6cad71bcaae725
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-05-06 15:49:51 -0700

    Write state shape into App file

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showSearch: true,
+      queryResults: [],
+      currentAlbum: {}
+    }
   }
 
   render() {
