52579edd3764419a494c03e51fab40d9862227b3
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-05-06 15:47:09 -0700

    Change App component to be stateful class component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-export default function(props) {
-  return (
-    <h1>Hello world!</h1>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hello world!</h1>
+    )
+  }
 }
+
+export default App;
