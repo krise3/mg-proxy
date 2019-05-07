1dbfa50b93cba3682a726ecc69bbd7caedbebdb8
Kristoffer U Enriquez <enriquezkristoffer@yahoo.com>
2019-05-07 11:44:47 -0700

    Connect to MongoDB and create document model

---
+++ database/model.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require('./connect.js');
+
+const albumSchema = new Schema({
+  label: String,
+  album_arist: String,
+  published: String,
+  catalog_number: String,
+  title: String,
+  additional_info: String,
+  songs: String,
+});
+
+let Album = model('Album', albumSchema);
+
+module.exports = Album;
